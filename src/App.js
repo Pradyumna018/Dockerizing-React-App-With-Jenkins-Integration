031037a8f25820e72a3f313bc5d5d7900190d7f2
Pradyumna Kumar <mohanty.subha1999@gmail.com>
2024-07-24 23:37:32 +0530

    React Jenkins CICD

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Pradyumna <code>Kumar</code> Mohanty - Subha Mohanty
+          Pradyumna <code>Kumar</code> Mohanty - Subha
         </p>
         <a
           className="App-link"
